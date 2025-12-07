5e937361c1df62ea3a6f3546727b80ed36f5627a
PEzwarts <pe.zwarts@tutamail.com>
2025-12-07 22:52:52 +0000

    Release

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <gtkmm.h>
+
+class win : public Gtk::Window {
+  public: Gtk::Window win {
+  };
+};
+
+int main(int argc, char **argv) {
+  auto app = Gtk::Application::create();
+
+  return app->make_window_and_run<win>(argc, argv);
+}
